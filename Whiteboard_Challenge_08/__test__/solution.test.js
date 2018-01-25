4510ff3be970caed6a2596ef07f76ec58403e265
Dean <dean.murphy90@gmail.com>
2018-01-24 20:30:21 -0800

    added whiteboard picture and tests

--- Whiteboard_Challenge_08/__test__/solution.test.js
+++ Whiteboard_Challenge_08/__test__/solution.test.js
@@ -6,7 +6,7 @@ require('jest');
 
 describe('Solution Module', function() {
   describe('#intersect', () => {
-    it('should return an sll with the intersected values', function() {
+    it('should return an sll with the intersected values', () => {
       let test = new SLL;
       let test2 = new SLL;
       test.insertEnd(1);
@@ -16,11 +16,12 @@ describe('Solution Module', function() {
       test2.insertEnd(3);
       expect(solution.intersect(test, test2)).toEqual(test2);
     });
-    it('arguments should be objects', function() {
 
+    it('arguments should be objects', () => {
       expect(solution.intersect(123, 345)).toBeNull();
+      
     });
-    it('should return an empty SLL if no intersections', function() {
+    it('should return an empty SLL if no intersections', () => {
       let test = new SLL;
       let test2 = new SLL;
       test.insertEnd(1);
